c03851f2ffafe31d08f9b2f52472b6fa0adb9f4c
yonei <yonei.sanchez@sitiouno.com>
2021-10-13 19:46:23 -0400

    Se implementa logica de bloqueo de botones para avanzar y retroceder

--- landing-page-test/src/components/Carousel.jsx
+++ landing-page-test/src/components/Carousel.jsx
@@ -1,15 +1,28 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import CarouselComponent from 'react-elastic-carousel'
 import arrowLeft from '../resource/arrow-left.svg';
 import arrowright from '../resource/arrow-right.svg';
 
 const Carousel = ({ children, ...props }) => {
-
+    const [disabledNext, setDisabledNext] = useState(false)
+    const [disabledPrev, setDisabledPrev] = useState(true)
     const ref = useRef(null)
 
     const handleNext = () => ref.current.slideNext()
 
     const hendlePrev = () => ref.current.slidePrev()
+
+    const onNextEnd = (currentItem, pageIndex) => {
+        let pages = ref.current.state.pages.length - 1;
+        setDisabledNext(pages === pageIndex)
+        setDisabledPrev(false)
+    }
+
+    const onPrevEnd = (currentItem, pageIndex) => {
+        setDisabledPrev(pageIndex === 0)
+        setDisabledNext(false)
+    }
+
     return (
         <div>
             <CarouselComponent
@@ -25,6 +38,8 @@ const Carousel = ({ children, ...props }) => {
                     { width: 1750, itemsToShow: 4 },
                 ]}
                 showArrows={false}
+                onNextEnd={onNextEnd}
+                onPrevEnd={onPrevEnd}
                 {...props} >
                 {
                     children
@@ -33,12 +48,19 @@ const Carousel = ({ children, ...props }) => {
             </ CarouselComponent>
 
             <div className="carousel-actions">
-                <button className="action" onClick={hendlePrev}>
+                <button
+                    className="action"
+                    onClick={hendlePrev}
+                    disabled={disabledPrev}
+                >
                     <img src={arrowLeft} />
                 </button>
-                <button className="action" onClick={handleNext}>
+                <button
+                    className="action"
+                    onClick={handleNext}
+                    disabled={disabledNext}
+                >
                     <img src={arrowright} />
-
                 </button>
             </div>
         </div>
