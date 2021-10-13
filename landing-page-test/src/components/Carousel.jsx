a2b57abbeb195de1a63bc196400b4fca2879ddbc
yonei <yonei.sanchez@sitiouno.com>
2021-10-13 18:57:52 -0400

    Se mejora responsive en dispositivos moviles

--- landing-page-test/src/components/Carousel.jsx
+++ landing-page-test/src/components/Carousel.jsx
@@ -4,41 +4,40 @@ import arrowLeft from '../resource/arrow-left.svg';
 import arrowright from '../resource/arrow-right.svg';
 
 const Carousel = ({ children, ...props }) => {
-   
+
     const ref = useRef(null)
 
-    const handleNext =() => ref.current.slideNext()
+    const handleNext = () => ref.current.slideNext()
 
     const hendlePrev = () => ref.current.slidePrev()
-  return (
+    return (
         <div>
-          <CarouselComponent 
-          ref={ref}
-          className="carousel"
-            itemsToShow={3}
-            pagination={false}
-            itemPadding={[80, 18]}
-            breakPoints={[
-                { width: 1, itemsToShow: 1 },
-                { width: 850, itemsToShow: 2},
-                { width: 1150, itemsToShow: 3, itemsToScroll: 2 },
-                { width: 1450, itemsToShow: 4 },
-                { width: 1750, itemsToShow: 5 },
-              ]}
-              showArrows={false}
-              {...props} >
-                    {
-                        children
-                    }
+            <CarouselComponent
+                ref={ref}
+                className="carousel"
+                itemsToShow={3}
+                pagination={false}
+                itemPadding={[80, 18]}
+                breakPoints={[
+                    { width: 1, itemsToShow: 1 },
+                    { width: 900, itemsToShow: 2 },
+                    { width: 1300, itemsToShow: 3, itemsToScroll: 2 },
+                    { width: 1750, itemsToShow: 4 },
+                ]}
+                showArrows={false}
+                {...props} >
+                {
+                    children
+                }
 
-        </ CarouselComponent>
+            </ CarouselComponent>
 
             <div className="carousel-actions">
                 <button className="action" onClick={hendlePrev}>
                     <img src={arrowLeft} />
                 </button>
                 <button className="action" onClick={handleNext}>
-                <img src={arrowright} />
+                    <img src={arrowright} />
 
                 </button>
             </div>
